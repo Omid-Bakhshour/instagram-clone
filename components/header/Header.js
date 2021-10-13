b5d4305b6b78f955a7aa7242d77cf35c02c41f47
Omid-Bakhshour <fishvardroid@gmail.com>
2021-10-13 18:00:35 +0330

    add header

---
+++ components/header/Header.js
@@ -0,0 +1,67 @@
+
+import Image from "next/image"
+
+import { SearchIcon, PlusCircleIcon, UserGroupIcon, HeartIcon, PaperAirplaneIcon, MenuIcon }
+    from "@heroicons/react/outline"
+import { HomeIcon }
+    from "@heroicons/react/solid"
+
+function Header() {
+    return (
+        <div className="shadow-sm border-b bg-white sticky top-0 z-50 " >
+
+            <div className="flex justify-between items-center bg-white max-w-6xl mx-5 xl:mx-auto">
+
+
+                <div className="relative w-24 h-10 hidden lg:inline-grid cursor-pointer flex-shrink-0 " >
+                    <Image src="/images/instaLogo.png" layout="fill" objectFit="contain" />
+
+                </div>
+
+                <div className="relative w-10 h-10 inline-grid lg:hidden  flex-shrink-0 cursor-pointer " >
+                    <Image src="/images/insta-logo2.png" layout="fill" objectFit="contain" />
+
+                </div>
+
+
+                <div className=" relative p-3 rounded-md ">
+                    <div className="absolute inset-y-0 pl-3 flex items-center pointer-events-none" >
+                        <SearchIcon className="h-5 w-5 text-gray-500" />
+
+                    </div>
+                    <input className="bg-gray-50 block w-full pl-10 sm:text-xm border-gray-300 rounded-md focus:ring-black
+                     focus:border-black " placeholder="Search" type="text" />
+                </div>
+
+                {/* right */}
+
+                <div className="flex items-center justify-end space-x-4">
+                    <HomeIcon className="navBtn" />
+                    <MenuIcon className="h-6 md:hidden cursor-pointer" />
+                    <div className="relative navBtn">
+                        <PaperAirplaneIcon className="navBtn rotate-45" />
+                        <div className="absolute -top-1 -right-2 text-xs w-5 h-5 bg-red-500 rounded-full flex items-center justify-center
+                        text-white animate-pulse">3</div>
+
+                    </div>
+                    <PlusCircleIcon className="navBtn" />
+                    <UserGroupIcon className="navBtn" />
+
+                    <HeartIcon className="navBtn" />
+
+                    <img src="https://lh3.googleusercontent.com/ogw/ADea4I63hAY1m6qVwise20ijlooItppdeTvwS52Yh1t1=s32-c-mo"
+                        className="rounded-full h-10 cursor-pointer " alt="profilepic" />
+
+
+                </div>
+
+
+            </div>
+
+
+
+        </div>
+    )
+}
+
+export default Header
